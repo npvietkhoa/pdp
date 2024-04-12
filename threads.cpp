abbef92d624bf24bd227efd3a6a8ab49581f50fe
npvietkhoa <npvietkhoa@gmail.com>
2024-04-12 13:53:10 +0200

    add examples creating threads with defined functions

--- threads.cpp
+++ threads.cpp
@@ -4,21 +4,40 @@
 #include <iostream>
 #include "thread"
 
+void foo_func() {
+    std::cout << "foo" << std::endl;
+    std::cout << "func_foo_id: " << std::this_thread::get_id() << std::endl;
+}
+
+void bar_func() {
+    std::cout << "bar" << std::endl;
+    std::cout << "func_bar_id: " << std::this_thread::get_id() << std::endl;
+}
+
 int main() {
     std::thread::id main_thread;
     std::cout << "main_id: " << main_thread << std::endl;
 
-    std::thread foo_thread([] {
+    // create threads with functions
+    std::thread foo_func_thread(foo_func);
+    std::thread bar_func_thread(bar_func);
+
+    // Create threads with lambda
+    std::thread foo_lambda_thread([] {
         std::cout << "foo_id: " << std::this_thread::get_id() << std::endl;
     });
-
-    std::thread bar_thread([] {
+    std::thread bar_lambda_thread([] {
         std::cout << "bar_id: " << std::this_thread::get_id() << std::endl;
     });
 
-//    Main threads will block and wait for other execution finish
-    foo_thread.join();
-    bar_thread.join();
+
+
+    // Main threads will block and wait for other execution finish
+    foo_func_thread.join();
+    bar_func_thread.join();
+
+    foo_func_thread.join();
+    bar_func_thread.join();
 
 //    execute independently of the thread handle
 //    foo_thread.detach();

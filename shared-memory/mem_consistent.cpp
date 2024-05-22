03cc527be9070215dfc57325cad3baeeddd768ee
npvietkhoa <npvietkhoa@gmail.com>
2024-05-22 16:38:58 +0200

    mem consistency

---
+++ shared-memory/mem_consistent.cpp
@@ -0,0 +1,30 @@
+//
+// Created by Khoa Nguyen on 22.05.24.
+//
+#include <iostream>
+#include "cassert"
+#include "thread"
+
+std::atomic<bool> flag(false);
+int               data;
+
+void foo_func() {
+    data = 22; // full fence - nf_data = 22 is visible to all threads
+    flag.store(true, std::memory_order_seq_cst);
+}
+
+void bar_func() {
+    // wait for nf_flag
+    while(!flag.load(std::memory_order_seq_cst)) {};
+
+    // full fence - load to assert can not be reordered.
+    assert(data == 22);
+}
+
+int mem_fence() {
+    std::thread foo_thrd(foo_func), bar_thread(bar_func);
+    foo_thrd.join();
+    bar_thread.join();
+}
+
+
---
+++ shared-memory/mem_consistent_no_fences.cpp
@@ -0,0 +1,34 @@
+//
+// Created by Khoa Nguyen on 22.05.24.
+//
+#include "iostream"
+#include "thread"
+#include "cassert"
+
+
+std::atomic<bool> nf_flag(false);
+int               nf_data;
+
+int mem_fence();
+
+void foo_nf_func() {
+    nf_data = 22;
+    nf_flag.store(true, std::memory_order_relaxed);
+}
+
+void bar_nf_func() {
+    while(!nf_flag.load(std::memory_order_relaxed)) {};
+
+    // could be failed here
+    assert(nf_data == 22);
+}
+
+
+int main() {
+    std::thread foo_thrd(foo_nf_func), bar_thrd(bar_nf_func);
+    foo_thrd.join();
+    bar_thrd.join();
+
+    mem_fence();
+}
+

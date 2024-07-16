13d5864bfb73a098020d63c8714625871cd8f9b4
npvietkhoa <npvietkhoa@gmail.com>
2024-07-16 16:34:04 +0200

    add test for threadsafe queue

--- shared-memory/tests/test_threadsafe_queue.cpp
+++ shared-memory/tests/test_threadsafe_queue.cpp
@@ -1,18 +1,85 @@
-#include "gtest/gtest.h"
+#include <gtest/gtest.h>
+#include <thread>
 #include "../threadsafe_queue.h"
 
-TEST(ThreadsafeQueueTest, BasicPushPop) {
+class ThreadSafeQueueTest : public ::testing::Test {
+protected:
     threadsafe_queue<int> queue;
-    int value = 42;
+};
 
-    // Push a value
-    queue.push(value);
+// Test for pushing and popping elements in single-threaded context
+TEST_F(ThreadSafeQueueTest, PushAndPop) {
+    queue.push(1);
+    queue.push(2);
 
-    // Wait and pop the value
-    int popped_value;
-    queue.wait_and_pop(popped_value);
-    EXPECT_EQ(value, popped_value);  // Check if popped value matches pushed value
+    int value;
+    queue.wait_and_pop(value);
+    EXPECT_EQ(value, 1);
+
+    queue.wait_and_pop(value);
+    EXPECT_EQ(value, 2);
 }
 
+// Test for thread safety using multiple threads
+TEST_F(ThreadSafeQueueTest, MultiThreadedPushAndPop) {
+    std::thread t1([&] {
+        for (int i = 0; i < 10; ++i) {
+            queue.push(i);
+        }
+    });
 
+    std::thread t2([&] {
+        for (int i = 10; i < 20; ++i) {
+            queue.push(i);
+        }
+    });
 
+    t1.join();
+    t2.join();
+
+    std::vector<int> popped_values;
+    for (int i = 0; i < 20; ++i) {
+        int value;
+        queue.wait_and_pop(value);
+        popped_values.push_back(value);
+    }
+
+    std::sort(popped_values.begin(), popped_values.end());
+    for (int i = 0; i < 20; ++i) {
+        EXPECT_EQ(popped_values[i], i);
+    }
+}
+
+// Test for copy constructor
+TEST_F(ThreadSafeQueueTest, CopyConstructor) {
+    queue.push(1);
+    queue.push(2);
+
+    threadsafe_queue<int> copied_queue(queue);
+
+    int value;
+    copied_queue.wait_and_pop(value);
+    EXPECT_EQ(value, 1);
+
+    copied_queue.wait_and_pop(value);
+    EXPECT_EQ(value, 2);
+}
+
+// Test for popping from an empty queue (waits for push)
+TEST_F(ThreadSafeQueueTest, WaitAndPopOnEmptyQueue) {
+    std::thread t([&] {
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        queue.push(42);
+    });
+
+    int value;
+    queue.wait_and_pop(value);
+    EXPECT_EQ(value, 42);
+
+    t.join();
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}

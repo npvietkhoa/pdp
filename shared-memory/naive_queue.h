7eb09a913d56b5871e3397a430ed5774ca1942b0
npvietkhoa <npvietkhoa@gmail.com>
2024-07-16 16:33:24 +0200

    update naive queue

--- shared-memory/naive_queue.h
+++ shared-memory/naive_queue.h
@@ -1,27 +1,65 @@
 #ifndef PDP_NAIVE_QUEUE_H
 #define PDP_NAIVE_QUEUE_H
 
-template<class T> class NaiveQueue {
+#include <stdexcept>  // For std::out_of_range
+#include <iostream>   // For std::cerr
+
+template<class T>
+class NaiveQueue {
 private:
     T* items;
-    int head, tail;
-    
+    int head, tail, capacity, size;
+
 public:
-    NaiveQueue(T *items, int head, int tail) : items(items), head(head), tail(tail) {}
-    
+    NaiveQueue(int capacity)
+            : items(new T[capacity]), head(0), tail(0), capacity(capacity), size(0) {}
+
+    ~NaiveQueue() {
+        delete[] items;
+    }
+
     void enq(T element);
-    
     T deq();
+    bool isEmpty() const;
+    bool isFull() const;
+    int getSize() const;
 };
 
 template<class T>
-T NaiveQueue<T>::deq() {
-    return NULL;
+void NaiveQueue<T>::enq(T element) {
+    if (isFull()) {
+        std::cerr << "Queue is full. Cannot enqueue element.\n";
+        return;
+    }
+    items[tail] = element;
+    tail = (tail + 1) % capacity;
+    ++size;
 }
 
 template<class T>
-void NaiveQueue<T>::enq(T element) {
-
+T NaiveQueue<T>::deq() {
+    if (isEmpty()) {
+        throw std::out_of_range("Queue is empty");
+    }
+    T element = items[head];
+    head = (head + 1) % capacity;
+    --size;
+    return element;
 }
 
-#endif //PDP_NAIVE_QUEUE_H
+template<class T>
+bool NaiveQueue<T>::isEmpty() const {
+    return size == 0;
+}
+
+template<class T>
+bool NaiveQueue<T>::isFull() const {
+    return size == capacity;
+}
+
+template<class T>
+int NaiveQueue<T>::getSize() const {
+    return size;
+}
+
+#endif // PDP_NAIVE_QUEUE_H

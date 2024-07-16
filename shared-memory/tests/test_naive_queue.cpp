b989369f589926595dfbdf5070132c9d18d7f492
npvietkhoa <npvietkhoa@gmail.com>
2024-07-16 16:33:12 +0200

    add tests for navie queue

--- shared-memory/tests/test_naive_queue.cpp
+++ shared-memory/tests/test_naive_queue.cpp
@@ -1,11 +1,66 @@
-//
-// Created by Khoa Nguyen on 12.04.24.
-//
-#include "gtest/gtest.h"
+#include <gtest/gtest.h>
 #include "../naive_queue.h"
 
-TEST(NaiveQueueTest, BasicCase) {
-    // ...
+class NaiveQueueTest : public ::testing::Test {
+protected:
+    NaiveQueue<int> *queue;
+
+    void SetUp() override {
+        queue = new NaiveQueue<int>(5);
+    }
+
+    void TearDown() override {
+        delete queue;
+    }
+};
+
+TEST_F(NaiveQueueTest, ConstructorTest) {
+    EXPECT_EQ(queue->getSize(), 0);
+    EXPECT_TRUE(queue->isEmpty());
+    EXPECT_FALSE(queue->isFull());
 }
 
+TEST_F(NaiveQueueTest, EnqueueTest) {
+    queue->enq(1);
+    EXPECT_EQ(queue->getSize(), 1);
+    EXPECT_FALSE(queue->isEmpty());
+    EXPECT_FALSE(queue->isFull());
+}
 
+TEST_F(NaiveQueueTest, DequeueTest) {
+    queue->enq(1);
+    int element = queue->deq();
+    EXPECT_EQ(element, 1);
+    EXPECT_EQ(queue->getSize(), 0);
+    EXPECT_TRUE(queue->isEmpty());
+    EXPECT_FALSE(queue->isFull());
+}
+
+TEST_F(NaiveQueueTest, EnqueueAndDequeueTest) {
+    queue->enq(1);
+    queue->enq(2);
+    EXPECT_EQ(queue->deq(), 1);
+    EXPECT_EQ(queue->deq(), 2);
+    EXPECT_TRUE(queue->isEmpty());
+}
+
+TEST_F(NaiveQueueTest, OverflowTest) {
+    queue->enq(1);
+    queue->enq(2);
+    queue->enq(3);
+    queue->enq(4);
+    queue->enq(5);
+    EXPECT_TRUE(queue->isFull());
+    // Try enqueuing another element
+    queue->enq(6);
+    EXPECT_EQ(queue->getSize(), 5);
+}
+
+TEST_F(NaiveQueueTest, UnderflowTest) {
+    EXPECT_THROW(queue->deq(), std::out_of_range);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
